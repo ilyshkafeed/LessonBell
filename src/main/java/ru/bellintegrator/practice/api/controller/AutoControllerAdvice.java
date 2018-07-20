dca989a26912145fc9bedd7a2935e93497a01a4d
ilyshka_fox <ilya_moslov@mail.ru>
2018-07-20 22:00:10 +0300

    Классы и методы посеченнае специальной анатацией при выводе оборачиваються в обект data

--- src/main/java/ru/bellintegrator/practice/api/controller/AutoControllerAdvice.java
+++ src/main/java/ru/bellintegrator/practice/api/controller/AutoControllerAdvice.java
@@ -19,6 +19,8 @@ public class AutoControllerAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        System.out.println(body.toString());
+        System.out.println(body.getClass());
         return new DataView(body);
     }
 }
--- src/main/java/ru/bellintegrator/practice/api/view/DataView.java
+++ src/main/java/ru/bellintegrator/practice/api/view/DataView.java
@@ -1,12 +1,16 @@
 package ru.bellintegrator.practice.api.view;
 
 public class DataView implements StaticView {
-    Object data;
+   private Object data;
 
     public DataView() {
     }
 
     public DataView(Object data) {
         this.data = data;
+    }
+
+    public Object getData() {
+        return data;
     }
 }
