e5ba4b3ee26d71571cecea380ee192d677f85db5
ilyshka_fox <ilya_moslov@mail.ru>
2018-07-28 02:42:40 +0300

    Обновил PhoneValidator Ужестожил правило на количество цифр.

--- src/main/java/ru/bellintegrator/practice/validator/PhoneConstraintValidator.java
+++ src/main/java/ru/bellintegrator/practice/validator/PhoneConstraintValidator.java
@@ -14,7 +14,7 @@ public class PhoneConstraintValidator implements ConstraintValidator<Phone, Stri
     @Override
     public boolean isValid(String phoneField, ConstraintValidatorContext cxt) {
         if (phoneField == null) return true;
-        String numberOnly = phoneField.replaceAll("[^0-9+]", "");
+        String numberOnly = phoneField.replaceAll("[^0-9]+", "");
         return (numberOnly.length() == 11)
                 && phoneField.matches("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$");
     }
